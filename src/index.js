113e31369f9279d1a368e6782dc1341dc1c4e7ef
aassiimmaa <nguyenthanhanvp2690@gmail.com>
2024-08-26 15:30:19 +0700

    he

--- src/index.js
+++ src/index.js
@@ -9,11 +9,9 @@ const port = 3000
 app.use(morgan('combined'))
 
 //Template engine
-app.engine('handlebars', handlebars.engine())
-app.set('view engine', 'handlebars')
-app.set('views', path.join(__dirname, 'resources/views'))
-
-console.log(path.join(__dirname, 'resources/views'))
+app.engine('hbs', handlebars.engine({extname: 'hbs'}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resource/views'))
 
 app.get('/', (req, res) => {
   res.render('home')
